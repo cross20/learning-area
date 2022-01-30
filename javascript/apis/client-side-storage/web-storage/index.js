d6922215e88fca74c4519c71d47f7a7b2e8f9037
Chad Ross <rosschad98@icloud.com>
2022-01-29 22:20:48 -0600

    Web storage

--- javascript/apis/client-side-storage/web-storage/index.js
+++ javascript/apis/client-side-storage/web-storage/index.js
@@ -1,4 +1,3 @@
-// create needed constants
 const rememberDiv = document.querySelector('.remember');
 const forgetDiv = document.querySelector('.forget');
 const form = document.querySelector('form');
@@ -9,48 +8,35 @@ const forgetBtn = document.querySelector('#forgetname');
 const h1 = document.querySelector('h1');
 const personalGreeting = document.querySelector('.personal-greeting');
 
-// Stop the form from submitting when a button is pressed
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
-// run function when the 'Say hello' button is clicked
-submitBtn.addEventListener('click', function() {
-  // store the entered name in web storage
+submitBtn.addEventListener('click', () => {
   localStorage.setItem('name', nameInput.value);
-  // run nameDisplayCheck() to sort out displaying the personalised greetings and updating the form display
   nameDisplayCheck();
 });
 
-// run function when the 'Forget' button is clicked
-forgetBtn.addEventListener('click', function() {
-  // Remove the stored name from web storage
+forgetBtn.addEventListener('click', () => {
   localStorage.removeItem('name');
-  // run nameDisplayCheck() to sort out displaying the generic greeting again and updating the form display
   nameDisplayCheck();
 });
 
-// define the nameDisplayCheck() function
 function nameDisplayCheck() {
-  // check whether the 'name' data item is stored in web Storage
-  if(localStorage.getItem('name')) {
-    // If it is, display personalized greeting
+  if (localStorage.getItem('name')) {
     let name = localStorage.getItem('name');
-    h1.textContent = 'Welcome, ' + name;
-    personalGreeting.textContent = 'Welcome to our website, ' + name + '! We hope you have fun while you are here.';
-    // hide the 'remember' part of the form and show the 'forget' part
+    h1.textContent = `Welcome ${name}`;
+    personalGreeting.textContent = `Welcome to our website, ${name}! We hope you have fun while you are here.`;
+
     forgetDiv.style.display = 'block';
     rememberDiv.style.display = 'none';
   } else {
-    // if not, display generic greeting
-    h1.textContent = 'Welcome to our website ';
-    personalGreeting.textContent = 'Welcome to our website. We hope you have fun while you are here.';
-    // hide the 'forget' part of the form and show the 'remember' part
+    h1.textContent = 'Welcome to our website';
+    personalGreeting.textContent = 'Welcome to our website, we hop you have fun while you are here.';
+
     forgetDiv.style.display = 'none';
     rememberDiv.style.display = 'block';
   }
 }
 
-// run nameDisplayCheck() when the DOM first loads to check wether a personal name was previously
-// set, and if so display the personalized greeting. If not, show the generic greeting
 document.body.onload = nameDisplayCheck;
